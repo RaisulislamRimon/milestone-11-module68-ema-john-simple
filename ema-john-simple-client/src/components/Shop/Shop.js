4e592782cc4b62b4b4c259fe4c2dd68c7d72f00e
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-05 16:24:57 +0600

    update

--- ema-john-simple-client/src/components/Shop/Shop.js
+++ ema-john-simple-client/src/components/Shop/Shop.js
@@ -88,6 +88,16 @@ const Shop = () => {
             {number}
           </button>
         ))}
+        <select onChange={(e) => setSize(e.target.value)} name="" id="">
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+          <option value="25">25</option>
+          <option value="30">30</option>
+          <option value="35">35</option>
+          <option value="40">40</option>
+        </select>
       </div>
     </div>
   );
