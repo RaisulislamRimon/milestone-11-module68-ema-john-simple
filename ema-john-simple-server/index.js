6ea3334f71d83b9d0bc87234365d5953c2722836
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-05 12:50:14 +0600

    setup server api to load all products data

--- ema-john-simple-server/index.js
+++ ema-john-simple-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,30 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb://localhost:27017`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ycs7beo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productCollection = client.db("emaJohnSimple").collection("products");
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send(`ema john server is running`);
