47d9b7b60dc8eec8c7d2cee7dcd9843babe0345f
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-05 12:57:41 +0600

    Add data from JSON to database and load on the UI

--- ema-john-simple-client/src/App.js
+++ ema-john-simple-client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
       children: [
         {
           path: "/",
-          loader: () => fetch("products.json"),
+          loader: () => fetch("http://localhost:5000/products"),
           element: <Shop />,
         },
         {
